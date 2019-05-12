ab964f612b50adcd15a3f003cde5a8bd94876fcf
avelreserch <pavel.oliynyk@gmail.com>
2019-05-12 14:26:39 +0100

    Finish week3 of BI

--- src/main/scala/SpectrumProblem.scala
+++ src/main/scala/SpectrumProblem.scala
@@ -2,7 +2,7 @@ import scala.io.Source
 
 object SpectrumProblem {
 
-  private val integetMassPath: String = "/Users/pavel/Sources/dna-analysis/src/main/resources/data/integer_mass_table.txt"
+  val integetMassPath: String = "/Users/pavel/Sources/dna-analysis/src/main/resources/data/integer_mass_table.txt"
 
   def massTable(): Map[String, String] =
     Source.fromFile(integetMassPath)
@@ -23,15 +23,35 @@ object SpectrumProblem {
 
   def main(args: Array[String]) = {
 
-    val peptide = "IEMHQWEYLIFS"
+//    val table = massTable()
+//    val rna = "CAACAG"
+//    val res = rna.map(c => table( c.toString() ).toInt ).sum
+//
+//    println(res)
+
+
+    val peptide = "TAIM"
     val variations = peptideVariations(peptide)
     val table = massTable()
     val res = (variations :+ peptide).map(item =>  item.map(c => table(c.toString).toInt).sum )
       .toList
       .sorted
 
+    println(res.mkString(" "))
+
     println( res.length )
-    println( "0 " + res.mkString(" ")  )
+    val spec = "0 71 101 113 131 184 202 214 232 285 303 315 345 416".split(" ").map(_.toInt)
+
+    val res2 = (0 +: res)
+
+    val r = spec.zip(res2).forall(p => p._1 == p._2)
+
+    println(r)
+
+
+    //assert(spec == res2)
+    //println( res.length )
+    //println( "0 " + res.mkString(" ")  )
 
     //println( variations.mkString(" ") )
 
