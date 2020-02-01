42fa1319605d17b7cf6d6d088b7a4978304a1f89
avelreserch <pavel.oliynyk@gmail.com>
2020-02-01 17:54:45 +0000

    C4-W4: implement unit test for graphSpectrum

--- src/main/scala/course4/Proteomics.scala
+++ src/main/scala/course4/Proteomics.scala
@@ -1,5 +1,7 @@
 package course4
 
+import course4.ProteomicsRunner.input
+
 object ProteomicsRunner extends App {
   import Proteomics._
 
@@ -7,24 +9,9 @@ object ProteomicsRunner extends App {
   //val inputText = "113 129 269 315 406 414 534 543 640 663 739 776 840 863 897 976 1025 1047 1138 1146 1235 1243 1334 1356 1405 1484 1518 1541 1605 1642 1718 1741 1838 1847 1967 1975 2066 2112 2252 2268 2381"
   val input = inputText.split(" ").map(_.toInt).toList
 
-  val data = for {
-    x <- 0 :: input
-    y <- 0 :: input
-    z = Math.abs(x - y)
-    if integer_mass_table.contains(z)
-  } yield {
-    if (x < y) (s"$x->$y", s"${integer_mass_table(z)}")
-    else (s"$y->$x", s"${integer_mass_table(z)}")
-  }
 
-//  val data = convolution(result.toArray).sorted
-  val dataMap = data.toMap
-  val dataVals = dataMap.keys.toList.sorted
-  dataVals.foreach(v => {
-    println(s"$v:${dataMap(v)}")
-  })
-
-
+  val graph = graphSpectrum(input)
+  graph.foreach(println)
 }
 
 object Proteomics {
@@ -47,10 +34,25 @@ object Proteomics {
     matrix.toArray
   }
 
-  def graphSpectrum(in: Seq[Int]): Seq[(Int, Int)] = {
+  def graphSpectrum(in: List[Int]): List[(Int, Int)] = {
+    val data = for {
+      x <- 0 :: in
+      y <- 0 :: in
+      z = Math.abs(x - y)
+      if integer_mass_table.contains(z)
+    } yield {
+      if (x < y) (s"$x->$y", s"${integer_mass_table(z)}")
+      else (s"$y->$x", s"${integer_mass_table(z)}")
+    }
 
-    Seq()
+    data.toMap
+      .map(r => r._1.split("->") )
+      .map(r => r.map(_.toInt) )
+      .toList.map(r => ( r(0), r(1) ) )
   }
 
+  def decodingIdealSpectrum(spectrum: Array[Int]) : String = {
+    ""
+  }
 
 }
--- src/test/scala/courser4/com/avel/dna/ProteomicsSpec.scala
+++ src/test/scala/courser4/com/avel/dna/ProteomicsSpec.scala
@@ -8,28 +8,22 @@ class ProteomicsSpec extends FlatSpec {
 
   it should "return Graph spectrum" in {
     val intputText = "57 71 154 185 301 332 415 429 486"
-    val input = intputText.split(" ").map(_.toInt)
+    val input = intputText.split(" ").map(_.toInt).toList
     val result = graphSpectrum(input)
-    val expectedResult =
-      """|0->57:G
-        |0->71:A
-        |57->154:P
-        |57->185:K
-        |71->185:N
-        |154->301:F
-        |185->332:F
-        |301->415:N
-        |301->429:K
-        |332->429:P
-        |415->486:A
-        |429->486:G
-        |""".stripMargin
-    val expectedA = expectedResult.split("\n")
-      .map(_.split(":"))
-      .map(row => ( row(0).split("->"), row(1) ) )
-    val expected = expectedA.map(r => (r._1(0).toInt, r._1(1).toInt ) )
-
-    //expected.foreach( println )
+    val expected = List(
+        (154,301),
+        (57,154),
+        (332,429),
+        (429,486),
+        (185,332),
+        (0,71),
+        (71,185),
+        (301,429),
+        (415,486),
+        (301,415),
+        (57,185),
+        (0,57)
+    )
     assert(expected === result)
   }
 
