26c4d632b693b3a31a6d6a8ad2ac45927aa726e0
avelreserch <pavel.oliynyk@gmail.com>
2020-02-09 11:18:06 +0000

    C4-W4: nj alog draft

--- src/main/scala/course4/neighborJoining.scala
+++ src/main/scala/course4/neighborJoining.scala
@@ -1,30 +1,68 @@
 package course4
 
-object neighborJoining extends App {
-  val D = Array(
-//        Array(0, 13, 21, 22),
-//        Array(13, 0, 12, 13),
-//        Array(21, 12, 0, 13),
-//        Array(22, 13, 13, 0)
-    Array(0, 13, 16, 10),
-    Array(13, 0, 21, 15),
-    Array(16, 21, 0, 18),
-    Array(10, 15, 18, 0)
+object c4week3Runner extends App {
+  type Matrix = Array[Array[Int]]
+
+  val d = Array(
+    Array(0, 13, 21, 22),
+    Array(13, 0, 12, 13),
+    Array(21, 12, 0, 13),
+    Array(22, 13, 13, 0)
   )
-  val totalDistance = D.map(_.sum)
-  val n = 4
-  val D_ = for {
+  val n = d.head.length
+  val nodes = (0 to n - 1).toSet[Int]
+
+  val totalDistance = d.map(_.sum)
+  val d_ = for {
     i <- 0 to n - 1
     j <- 0 to n - 1
+  } yield if (i != j) (n - 2) * d(i)(j) - totalDistance(i) - totalDistance(j) else 0
+
+  val result = d_.sliding(4, 4).toArray
+
+  result.foreach(row => println(row.mkString(" ")) )
+
+  val ms = minPair(result.map(_.toArray), n)
+  //println(ms)
+  println(nodes -- ms)
+
+  var mr = Map[(Int, Int), Int]()
+  // TODO: convert merge set into list of pair of coordinates
+  (nodes -- ms).sliding(2, 2).foreach(p => {
+    val (x, y) = (p.head, p.last)
+    mr = mr + ((x, y ) -> d(x)(y) )
+  })
+
+  for {
+    x <- (nodes -- ms).toList
+    row = ( ms.map(i => d(x)(i) ).sum  - d( ms.head )( ms.last ) ) / 2
   } yield {
-    if (i == j )
-      0
-    else
-      (n - 2) * D(i)(j) - totalDistance(i) - totalDistance(j)
+     mr = mr + ((x, ms.last) -> row)
   }
 
- val F = D_.sliding(n, n).toArray
+  mr.foreach(r => println(r))
 
- F.foreach(row => println(row.mkString(" ")))
+
+
+  def minPair(mx: Matrix, N: Int ) : Set[Int] = {
+    val minElement : Int = mx.flatten.min
+    println(minElement)
+      for (i <- 0 to N - 1) {
+        for (j <- 0 to N - 1) {
+          if (mx(i)(j) == minElement)
+            return Set(i, j)
+        }
+      }
+    Set(0, 0)
+  }
+
+  println("This is not a test")
+}
+
+object TreeReconstruction {
+
+  def neighborJoining() : Unit = {
+
+  }
 
 }
