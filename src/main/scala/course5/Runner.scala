c0792bd7222c3bddca6f6f139ea95767dd40ddc2
avelreserch <pavel.oliynyk@gmail.com>
2020-03-08 19:29:39 +0000

    C5-W1: tidy up

--- src/main/scala/course5/Runner.scala
+++ src/main/scala/course5/Runner.scala
@@ -2,40 +2,24 @@ package course5
 
 import course5.Cluster._
 
-import scala.util.Try
-
 object Runner extends App {
 
-  val lines = scala.io.Source.fromFile("/Users/pavel/devcore/sources/dna-analysis/src/main/resources/data/distortion.txt")
-    .getLines().toList
+//  val data = Seq(Point(Seq(2, 6)), Point(Seq(4, 9)),
+//    Point(Seq(5, 7)), Point(Seq(6, 5)), Point(Seq(8, 3)))
+//
+//  val centers = Seq(Point(Seq(4, 5)), Point(Seq(7, 4)))
+//
+//  val actual = maxDistance(data, centers)._1
+//
+//  println(actual)
 
-  val (k, m) = {
-    val r = lines.head.split(" ").map(_.toInt)
-    (r(0), r(1) )
-  }
-
-  var isCenter = true
-  val in = lines.tail
-    .map( _.split(" ") )
-    .map( row => Try { Point( row.toSeq.map(_.toDouble)) }.toOption )
-    .map(e => e match {
-      case Some(p) => (Some(p), isCenter)
-      case None => {
-        isCenter = !isCenter
-        (None, isCenter)
-      }
-    }).filter(e => e._1.isDefined)
-  val (centers, data) = {
-    val (a, b) = in.partition(e => e._2 == true)
-    (a.map(_._1.get), b.map(_._1.get))
-  }
-
-  val result = distortion(data, centers)
-  println(result)
-
-  //val acutal = farthesFirstTraversal(k, m, in)
-  //acutal.foreach(p => {
-  //  println(p.dimPos.mkString(" "))
-  //})
+//    val data = Seq( Point( Seq(2, 8) ), Point( Seq(2, 5) ),
+//      Point( Seq(6, 9) ), Point( Seq(7, 5) ), Point( Seq(5, 2) ) )
+//
+//    val centers = Seq( Point( Seq(3, 5) ), Point( Seq(5, 4) )  )
+//
+//    val actual = distortion(data, centers)
+//
+//    println(actual)
 
 }
