ebd812e3c181fdae2ba216c407c803ffa627d166
avelreserch <pavel.oliynyk@gmail.com>
2020-12-27 17:39:57 +0000

    Implement TriMatching

--- src/main/scala/course6/DnaMutation.scala
+++ src/main/scala/course6/DnaMutation.scala
@@ -38,11 +38,46 @@ object DnaMutation {
     root
   }
 
-  def formattedPrint(current: Node) : String = {
-      current
+  def formattedPrint(implicit trie: Node) : String = {
+    trie
         .children
-        .map(c => s"\n${current.index}->${c._2.index}:${c._1}").mkString("") +
-        current.children.map( kv => formattedPrint(kv._2)).mkString("")
+        .map(c => s"\n${trie.index}->${c._2.index}:${c._1}").mkString("") +
+      trie.children.map( kv => formattedPrint(kv._2)).mkString("")
   }
 
+  /* Returns: matching text prefix with any patterns or None if no match found */
+  def prefixTrieMatching(textPair: (String, Int), trie: Node): Option[(String, Int)] = {
+
+    def prefixTrieMatchingAcc(key: Char, current: Node, input: String, res: List[Char]) : Option[String] = {
+      if (!current.children.isEmpty){
+        if (current.children.contains(key)){
+          if (input.length >= 1)
+            prefixTrieMatchingAcc(input.head, current.children.get(key).get, if (input.length > 1) input.tail else "", res :+ key)
+          else
+            if (res.length > 0 && current.children.get(key).get.children.isEmpty )
+              Some(res.mkString(""))
+            else
+              None
+        } else
+          None
+      } else {
+        if (res.length > 0) Some(res.mkString("")) else None
+      }
+    }
+//      if (!current.children.isEmpty && current.children.contains(key) && input.length > 1)
+//        prefixTrieMatchingAcc(input.head, current.children.get(key).get, input.tail, res :+ key)
+//      else if (!current.children.isEmpty)
+//        None
+//      else
+//        Some(res.mkString(""))
+
+    prefixTrieMatchingAcc(textPair._1.head, trie, textPair._1.tail, List.empty)
+      .map( (_, textPair._2) )
+  }
+
+  def slideText(input: (String, Int), acc: List[(String, Int)]) : List[(String, Int)] = if (input._1.length > 1){
+    slideText((input._1.tail, input._2 + 1), acc :+ input)
+  } else {
+    acc
+  }
 }
--- src/main/scala/course6/Runner.scala
+++ src/main/scala/course6/Runner.scala
@@ -1,26 +1,53 @@
 package course6
 
 object Runner extends App {
-  val input = scala.io.Source
-    .fromFile("/Users/pavel/devcore/sources/dna-analysis/src/test/resources/data/patterns.txt")
-    .getLines().toList
-//  val input = List("ATAGA","ATC","GAT")
-  val actual = DnaMutation.createTrie(input)
-  val rows = DnaMutation.formattedPrint(actual)
-    .split("\n")
+  import  DnaMutation._
 
-  val result = rows
-    .map(_.split(":"))
-    .filter(_.length == 2)
-    .map(arr => (arr(0), arr(1)) )
-    .map(p => (p._1.split("->"), p._2) )
-    .map(t => (t._1(0).toInt, t._1(1).toInt, t._2))
-    .sortBy(_._1)
-    .map(t => s"${t._1}->${t._2}:${t._3}")
-    .mkString("\n")
+  val data = scala.io.Source
+    .fromFile("/Users/pavel/devcore/sources/dna-analysis/src/test/resources/data/triematch.txt")
+    .getLines().toList
+
+//  val input = List("ATCG", "GGGT")
+//  val text = "AATCGGGTTCAATCGGGGT"
+  val text = data.head
+
+  implicit val trie = createTrie(data.tail)
+
+  //println(formattedPrint)
+
+  val slides = slideText( (text, 0), List.empty)
+  //println(slides)
+  val slides2 = List(("GGGT",15))
+
+  val res = slides
+    .map( prefixTrieMatching(_, trie))
+    .filter(_.isDefined)
+
+  println( res
+    .map(s => s.map(_._2))
+    .filter(_.isDefined)
+    .map(_.get)
+    .mkString(" ")
+  )
+
+  //val actual = prefixTrieMatching(text, trie)
+  //println(actual)
+
+  //val rows = DnaMutation.formattedPrint(actual)
+  //  .split("\n")
+
+//  val result = rows
+//    .map(_.split(":"))
+//    .filter(_.length == 2)
+//    .map(arr => (arr(0), arr(1)) )
+//    .map(p => (p._1.split("->"), p._2) )
+//    .map(t => (t._1(0).toInt, t._1(1).toInt, t._2))
+//    .sortBy(_._1)
+//    .map(t => s"${t._1}->${t._2}:${t._3}")
+//    .mkString("\n")
 //    .sortBy(_._1)
 //    .map(p => s"${p._1}:${p._2}")
 //    .mkString("\n")
+//  println( result )
 
-  println( result )
 }
