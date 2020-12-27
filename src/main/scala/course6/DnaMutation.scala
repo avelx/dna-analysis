85b5090d4384624495c62ad247d219520c5a617a
avelreserch <pavel.oliynyk@gmail.com>
2020-12-27 13:14:51 +0000

    Working version of trie construct: TODO: unit test

--- src/main/scala/course6/DnaMutation.scala
+++ src/main/scala/course6/DnaMutation.scala
@@ -7,6 +7,8 @@ final case class Node(key: Char, index: Int, children : mutable.Map[Char, Node])
 
 object DnaMutation {
 
+  var totalIndex = 1
+
   private def addPattern(current: Node, pattern: String): Node = pattern.headOption match {
     case Some(key) if current.children.contains(key) =>
       addPattern(current.children.get(key).get, pattern.tail)
@@ -21,7 +23,8 @@ object DnaMutation {
     if (current.children.contains(key)) // no need to add node
       current
     else {
-      val childNode = Node(key, current.index + 1, mutable.Map[Char, Node]().empty)
+      val childNode = Node(key, totalIndex, mutable.Map[Char, Node]().empty)
+      totalIndex += 1
       val childrenUpdated = current.children += (key -> childNode)
       val updated = current.copy( children = childrenUpdated )
       updated.children.get(key).get
@@ -36,7 +39,10 @@ object DnaMutation {
   }
 
   def formattedPrint(current: Node) : String = {
-    s"${current.key}:${current.index}" + " -> " + current.children.map( kv => formattedPrint(kv._2)).mkString("\n")
+      current
+        .children
+        .map(c => s"\n${current.index}->${c._2.index}:${c._1}").mkString("") +
+        current.children.map( kv => formattedPrint(kv._2)).mkString("")
   }
 
 }
--- src/main/scala/course6/Runner.scala
+++ src/main/scala/course6/Runner.scala
@@ -1,7 +1,26 @@
 package course6
 
 object Runner extends App {
-  val input = List("ATAGA","ATC","GAT")
+  val input = scala.io.Source
+    .fromFile("/Users/pavel/devcore/sources/dna-analysis/src/test/resources/data/patterns.txt")
+    .getLines().toList
+//  val input = List("ATAGA","ATC","GAT")
   val actual = DnaMutation.createTrie(input)
-  println( DnaMutation.formattedPrint(actual) )
+  val rows = DnaMutation.formattedPrint(actual)
+    .split("\n")
+
+  val result = rows
+    .map(_.split(":"))
+    .filter(_.length == 2)
+    .map(arr => (arr(0), arr(1)) )
+    .map(p => (p._1.split("->"), p._2) )
+    .map(t => (t._1(0).toInt, t._1(1).toInt, t._2))
+    .sortBy(_._1)
+    .map(t => s"${t._1}->${t._2}:${t._3}")
+    .mkString("\n")
+//    .sortBy(_._1)
+//    .map(p => s"${p._1}:${p._2}")
+//    .mkString("\n")
+
+  println( result )
 }
